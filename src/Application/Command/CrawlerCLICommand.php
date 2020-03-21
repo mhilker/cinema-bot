a466903381016b09734f34b6e0c036b0f6e95c75
Maik Hilker <maik@hilker.io>
2020-03-21 18:30:59 +0100

    refactoring

--- src/Application/Command/CrawlerCLICommand.php
+++ src/Application/Command/CrawlerCLICommand.php
@@ -7,6 +7,7 @@ namespace CinemaBot\Application\Command;
 use CinemaBot\Application\CQRS\CommandBus;
 use CinemaBot\Domain\CinemaList\CinemaListProjection;
 use CinemaBot\Domain\CrawlCinema\CrawlCinemaCommand;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -15,12 +16,14 @@ final class CrawlerCLICommand extends Command
 {
     private CommandBus $commandBus;
     private CinemaListProjection $projection;
+    private LoggerInterface $logger;
 
-    public function __construct(CommandBus $commandBus, CinemaListProjection $projection)
+    public function __construct(CommandBus $commandBus, CinemaListProjection $projection, LoggerInterface $logger)
     {
         parent::__construct();
         $this->commandBus = $commandBus;
         $this->projection = $projection;
+        $this->logger = $logger;
     }
 
     protected function configure(): void
@@ -33,6 +36,7 @@ final class CrawlerCLICommand extends Command
         $cinemaIDs = $this->projection->load();
 
         foreach ($cinemaIDs as $cinemaID) {
+            $this->logger->info('Crawling cinema', ['id' => $cinemaID->asString()]);
             $this->commandBus->dispatch(new CrawlCinemaCommand($cinemaID));
         }
 
--- src/Domain/SendNotifications/NotifierSystem.php
+++ src/Domain/SendNotifications/NotifierSystem.php
@@ -4,27 +4,38 @@ declare(strict_types=1);
 
 namespace CinemaBot\Domain\SendNotifications;
 
+use Psr\Log\LoggerInterface;
+
 final class NotifierSystem
 {
     private NotificationProjection $projection;
     private Notifier $notifier;
+    private LoggerInterface $logger;
 
-    public function __construct(NotificationProjection $projection, Notifier $notifier)
+    public function __construct(NotificationProjection $projection, Notifier $notifier, LoggerInterface $logger)
     {
         $this->projection = $projection;
         $this->notifier = $notifier;
+        $this->logger = $logger;
     }
 
     public function run(): void
     {
         $notifications = $this->projection->fetch();
+        $this->logger->info('{count} notification(s) found', ['count' => $notifications->count()]);
 
         foreach ($notifications as $notification) {
             $id = $notification->getNotificationID();
             try {
+                $this->logger->info('Sending notification', ['id' => $id->asString()]);
                 $this->notifier->notify($notification);
                 $this->projection->ack($id);
+                $this->logger->info('Acked notification', ['id' => $id->asString()]);
             } catch (\Exception $exception) {
+                $this->logger->error('Could not send notification', [
+                    'id' => $id->asString(),
+                    'exception' => $exception,
+                ]);
                 $this->projection->nack($id);
             }
         }
