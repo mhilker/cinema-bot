b2dd6c7d77ad69dfaef8074f7aafde75d0803e7a
Maik Hilker <maik@hilker.io>
2020-03-28 13:00:00 +0100

    updated nginx

--- docker/nginx/entrypoint.sh
+++ docker/nginx/entrypoint.sh
@@ -3,4 +3,4 @@
 cp /etc/nginx/default.tmpl.conf /etc/nginx/conf.d/default.conf
 sed -i "s/PHP_HOSTNAME/${PHP_HOSTNAME}/" /etc/nginx/conf.d/default.conf
 
-nginx -g "daemon off;"
+exec nginx -g "daemon off;"
