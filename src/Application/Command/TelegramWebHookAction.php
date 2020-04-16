70aa2e1c00fd6126bfd49bc87bde0e7fa58e3797
Maik Hilker <gpg@hilker.io>
2020-04-16 21:50:46 +0200

    catched exception on telegram webhook

--- src/Application/Command/TelegramWebHookAction.php
+++ src/Application/Command/TelegramWebHookAction.php
@@ -7,22 +7,31 @@ namespace CinemaBot\Application\Command;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
+use Psr\Log\LoggerInterface;
 use Slim\Psr7\Response;
 use TelegramBot\Api\Client;
+use Throwable;
 
 final class TelegramWebHookAction implements RequestHandlerInterface
 {
     private Client $telegram;
+    private LoggerInterface $logger;
 
-    public function __construct(Client $telegram)
+    public function __construct(Client $telegram, LoggerInterface $logger)
     {
         $this->telegram = $telegram;
+        $this->logger = $logger;
     }
 
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
-        // TODO: inject request
-        $this->telegram->run();
+        try {
+            // TODO: inject request
+            $this->telegram->run();
+        } catch (Throwable $error) {
+            $this->logger->error('An error occurred', ['error' => $error]);
+            return new Response(500);
+        }
 
         return new Response(200);
     }
